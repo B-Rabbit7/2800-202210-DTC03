3e42cc98db1216c9c826bb25b54ffa79cb0267f5
Joseph Chan <josephchan36@gmail.com>
2022-05-03 13:58:21 -0700

    Updated skeleton

---
+++ scripts/skeleton.js
@@ -0,0 +1,14 @@
+loadSkeleton();
+function loadSkeleton(){
+    console.log($('#headerPlaceholder').load('../text/header.html'));
+    console.log($('#footerPlaceholder').load('../text/footer.html'));
+}
+function openNav() {
+  console.log("called opennav")
+    document.getElementById("myNav").style.height = "100%";
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+  }
